43ec693153ba91600c0bc357aa4254651038fe10
Bartosz Skrzydelski <73198283+BrtSkr@users.noreply.github.com>
2022-06-08 02:39:50 +0200

    Many changes (Check description)
    -Added animations
    -New navbar
    -Small changes to header, footer, mediaQueries
    -Added tooltip with information about who got killed and by who in section death (Hover head)
    -Switched to good ol parcel-bundler

--- build/index.d02d1050.js
+++
@@ -1,2 +0,0 @@
-const closeOpenNav=(e,o)=>{var n=!1;o.addEventListener("click",(()=>{n?(e.style.transform="translateX(-20rem)",n=!n,console.log(n)):(e.style.transform="translateX(-1.5rem)",n=!n,console.log(n))}))};closeOpenNav(document.querySelector(".nav-subholder"),document.querySelector(".nav-closeOpen-button"));
-//# sourceMappingURL=index.d02d1050.js.map
--- dist/index.de5c0784.js
+++
@@ -1,6 +0,0 @@
-const x = ()=>{
-    console.log(document.querySelector(".nav-subholder"));
-};
-x();
-
-//# sourceMappingURL=index.de5c0784.js.map
